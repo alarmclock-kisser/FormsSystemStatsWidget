a18cd751dd8f5b18a52e0d60bf45d0ad3dfbbfa1
Marcel King <marcel.king91299@gmail.com>
2026-02-19 22:42:47 +0100

    Projektdateien hinzufügen.

---
+++ FormsSystemStatsWidget.Forms/WindowWidget.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace FormsSystemStatsWidget.Forms
+{
+    partial class WindowWidget
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox_cpu = new PictureBox();
+            this.progressBar_ram = new ProgressBar();
+            this.label_ram = new Label();
+            this.contextMenuStrip_widget = new ContextMenuStrip(this.components);
+            this.updateIntervalToolStripMenuItem = new ToolStripMenuItem();
+            this.toolStripTextBox_interval = new ToolStripTextBox();
+            this.selectGPUToolStripMenuItem = new ToolStripMenuItem();
+            this.toolStripComboBox_gpus = new ToolStripComboBox();
+            this.diagramColorToolStripMenuItem = new ToolStripMenuItem();
+            this.toolStripTextBox_diagramColor = new ToolStripTextBox();
+            this.showUsageToolStripMenuItem = new ToolStripMenuItem();
+            this.toolStripTextBox_percentageColor = new ToolStripTextBox();
+            this.label_vram = new Label();
+            this.progressBar_vram = new ProgressBar();
+            this.label_wattage = new Label();
+            this.label_gpuUsage = new Label();
+            ((System.ComponentModel.ISupportInitialize) this.pictureBox_cpu).BeginInit();
+            this.contextMenuStrip_widget.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox_cpu
+            // 
+            this.pictureBox_cpu.BackColor = SystemColors.ActiveBorder;
+            this.pictureBox_cpu.Dock = DockStyle.Top;
+            this.pictureBox_cpu.Location = new Point(0, 0);
+            this.pictureBox_cpu.Name = "pictureBox_cpu";
+            this.pictureBox_cpu.Size = new Size(240, 100);
+            this.pictureBox_cpu.TabIndex = 0;
+            this.pictureBox_cpu.TabStop = false;
+            // 
+            // progressBar_ram
+            // 
+            this.progressBar_ram.Location = new Point(0, 121);
+            this.progressBar_ram.Maximum = 1000;
+            this.progressBar_ram.Name = "progressBar_ram";
+            this.progressBar_ram.Size = new Size(240, 12);
+            this.progressBar_ram.TabIndex = 1;
+            // 
+            // label_ram
+            // 
+            this.label_ram.AutoSize = true;
+            this.label_ram.Location = new Point(0, 103);
+            this.label_ram.Name = "label_ram";
+            this.label_ram.Size = new Size(44, 15);
+            this.label_ram.TabIndex = 2;
+            this.label_ram.Text = "RAM: -";
+            // 
+            // contextMenuStrip_widget
+            // 
+            this.contextMenuStrip_widget.Items.AddRange(new ToolStripItem[] { this.updateIntervalToolStripMenuItem, this.selectGPUToolStripMenuItem, this.diagramColorToolStripMenuItem, this.showUsageToolStripMenuItem });
+            this.contextMenuStrip_widget.Name = "contextMenuStrip_widget";
+            this.contextMenuStrip_widget.Size = new Size(181, 114);
+            this.contextMenuStrip_widget.Text = "Settings";
+            // 
+            // updateIntervalToolStripMenuItem
+            // 
+            this.updateIntervalToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { this.toolStripTextBox_interval });
+            this.updateIntervalToolStripMenuItem.Name = "updateIntervalToolStripMenuItem";
+            this.updateIntervalToolStripMenuItem.Size = new Size(180, 22);
+            this.updateIntervalToolStripMenuItem.Text = "Update Interval ...";
+            // 
+            // toolStripTextBox_interval
+            // 
+            this.toolStripTextBox_interval.Name = "toolStripTextBox_interval";
+            this.toolStripTextBox_interval.Size = new Size(100, 23);
+            this.toolStripTextBox_interval.Text = "250";
+            this.toolStripTextBox_interval.TextChanged += this.toolStripTextBox_interval_TextChanged;
+            // 
+            // selectGPUToolStripMenuItem
+            // 
+            this.selectGPUToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { this.toolStripComboBox_gpus });
+            this.selectGPUToolStripMenuItem.Name = "selectGPUToolStripMenuItem";
+            this.selectGPUToolStripMenuItem.Size = new Size(180, 22);
+            this.selectGPUToolStripMenuItem.Text = "Select GPU ...";
+            // 
+            // toolStripComboBox_gpus
+            // 
+            this.toolStripComboBox_gpus.Name = "toolStripComboBox_gpus";
+            this.toolStripComboBox_gpus.Size = new Size(121, 23);
+            this.toolStripComboBox_gpus.Text = "0";
+            this.toolStripComboBox_gpus.SelectedIndexChanged += this.toolStripComboBox_gpus_SelectedIndexChanged;
+            // 
+            // diagramColorToolStripMenuItem
+            // 
+            this.diagramColorToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { this.toolStripTextBox_diagramColor });
+            this.diagramColorToolStripMenuItem.Name = "diagramColorToolStripMenuItem";
+            this.diagramColorToolStripMenuItem.Size = new Size(180, 22);
+            this.diagramColorToolStripMenuItem.Text = "Diagram Color ...";
+            // 
+            // toolStripTextBox_diagramColor
+            // 
+            this.toolStripTextBox_diagramColor.Name = "toolStripTextBox_diagramColor";
+            this.toolStripTextBox_diagramColor.Size = new Size(100, 23);
+            this.toolStripTextBox_diagramColor.Text = "#ffffff";
+            this.toolStripTextBox_diagramColor.DoubleClick += this.toolStripTextBox_diagramColor_DoubleClick;
+            this.toolStripTextBox_diagramColor.TextChanged += this.toolStripTextBox_diagramColor_TextChanged;
+            // 
+            // showUsageToolStripMenuItem
+            // 
+            this.showUsageToolStripMenuItem.Checked = true;
+            this.showUsageToolStripMenuItem.CheckOnClick = true;
+            this.showUsageToolStripMenuItem.CheckState = CheckState.Checked;
+            this.showUsageToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { this.toolStripTextBox_percentageColor });
+            this.showUsageToolStripMenuItem.Name = "showUsageToolStripMenuItem";
+            this.showUsageToolStripMenuItem.Size = new Size(180, 22);
+            this.showUsageToolStripMenuItem.Text = "Show Per Core % ...";
+            // 
+            // toolStripTextBox_percentageColor
+            // 
+            this.toolStripTextBox_percentageColor.Name = "toolStripTextBox_percentageColor";
+            this.toolStripTextBox_percentageColor.Size = new Size(100, 23);
+            this.toolStripTextBox_percentageColor.Text = "#8a2be2 ";
+            this.toolStripTextBox_percentageColor.DoubleClick += this.toolStripTextBox_percentageColor_DoubleClick;
+            this.toolStripTextBox_percentageColor.EnabledChanged += this.toolStripTextBox_percentageColor_EnabledChanged;
+            this.toolStripTextBox_percentageColor.TextChanged += this.toolStripTextBox_percentageColor_TextChanged;
+            // 
+            // label_vram
+            // 
+            this.label_vram.AutoSize = true;
+            this.label_vram.Location = new Point(0, 160);
+            this.label_vram.Name = "label_vram";
+            this.label_vram.Size = new Size(51, 15);
+            this.label_vram.TabIndex = 4;
+            this.label_vram.Text = "VRAM: -";
+            // 
+            // progressBar_vram
+            // 
+            this.progressBar_vram.Location = new Point(0, 178);
+            this.progressBar_vram.Maximum = 1000;
+            this.progressBar_vram.Name = "progressBar_vram";
+            this.progressBar_vram.Size = new Size(240, 12);
+            this.progressBar_vram.TabIndex = 3;
+            // 
+            // label_wattage
+            // 
+            this.label_wattage.AutoSize = true;
+            this.label_wattage.Location = new Point(0, 145);
+            this.label_wattage.Name = "label_wattage";
+            this.label_wattage.Size = new Size(48, 15);
+            this.label_wattage.TabIndex = 5;
+            this.label_wattage.Text = "Watts: -";
+            // 
+            // label_gpuUsage
+            // 
+            this.label_gpuUsage.AutoSize = true;
+            this.label_gpuUsage.Location = new Point(166, 145);
+            this.label_gpuUsage.Name = "label_gpuUsage";
+            this.label_gpuUsage.Size = new Size(41, 15);
+            this.label_gpuUsage.TabIndex = 6;
+            this.label_gpuUsage.Text = "GPU: -";
+            // 
+            // WindowWidget
+            // 
+            this.AutoScaleDimensions = new SizeF(7F, 15F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(240, 193);
+            this.ContextMenuStrip = this.contextMenuStrip_widget;
+            this.Controls.Add(this.label_gpuUsage);
+            this.Controls.Add(this.label_wattage);
+            this.Controls.Add(this.label_vram);
+            this.Controls.Add(this.progressBar_vram);
+            this.Controls.Add(this.label_ram);
+            this.Controls.Add(this.progressBar_ram);
+            this.Controls.Add(this.pictureBox_cpu);
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new Size(256, 232);
+            this.MinimumSize = new Size(256, 232);
+            this.Name = "WindowWidget";
+            this.Text = "System Statistics";
+            ((System.ComponentModel.ISupportInitialize) this.pictureBox_cpu).EndInit();
+            this.contextMenuStrip_widget.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox_cpu;
+        private ProgressBar progressBar_ram;
+        private Label label_ram;
+        private ContextMenuStrip contextMenuStrip_widget;
+        private ToolStripMenuItem updateIntervalToolStripMenuItem;
+        private ToolStripTextBox toolStripTextBox_interval;
+        private Label label_vram;
+        private ProgressBar progressBar_vram;
+        private Label label_wattage;
+        private Label label_gpuUsage;
+        private ToolStripMenuItem selectGPUToolStripMenuItem;
+        private ToolStripComboBox toolStripComboBox_gpus;
+        private ToolStripMenuItem diagramColorToolStripMenuItem;
+        private ToolStripTextBox toolStripTextBox_diagramColor;
+        private ToolStripMenuItem showUsageToolStripMenuItem;
+        private ToolStripTextBox toolStripTextBox_percentageColor;
+    }
+}
---
+++ FormsSystemStatsWidget.Forms/WindowWidget.cs
@@ -0,0 +1,202 @@
+using FormsSystemStatsWidget.Core;
+using Timer = System.Windows.Forms.Timer;
+
+namespace FormsSystemStatsWidget.Forms
+{
+    public partial class WindowWidget : Form
+    {
+        private int _updateIntervalMs = 250;
+        private Color _diagramColor = Color.White;
+        private Color? _percentageColor = Color.BlueViolet;
+
+        private Timer UpdateTimer;
+        private GpuStats Gpu;
+
+
+        public WindowWidget()
+        {
+            this.InitializeComponent();
+
+            this.UpdateTimer = new Timer();
+            this.UpdateTimer.Interval = this._updateIntervalMs;
+            this.UpdateTimer.Tick += this.Timer_Tick;
+            this.UpdateTimer.Start();
+
+            this.toolStripComboBox_gpus.Items.Clear();
+            this.toolStripComboBox_gpus.Items.AddRange(GpuStats.GpuNames.ToArray());
+            if (this.toolStripComboBox_gpus.Items.Count > 0)
+            {
+                this.toolStripComboBox_gpus.SelectedIndex = 0;
+            }
+
+            this.Gpu = new GpuStats(this.toolStripComboBox_gpus.SelectedIndex);
+
+            this.MouseDown += (s, e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    this.Capture = false;
+                    Message m = Message.Create(this.Handle, 0xA1, new IntPtr(2), IntPtr.Zero);
+                    this.WndProc(ref m);
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+                    this.contextMenuStrip_widget.Show(this, e.Location);
+                }
+            };
+        }
+
+        private async void Timer_Tick(object? sender, EventArgs e)
+        {
+            var threads = CpuStats.GetThreadUsages();
+            double ramTotalGb = Math.Round(CpuStats.GetTotalMemoryBytes() / 1_073_741_824.0, 3);
+            double ramUsedGb = Math.Round((CpuStats.GetUsedMemoryBytes()) / 1_073_741_824.0, 3);
+
+            double gpuUsage = this.Gpu.CurrentLoad01 * 100;
+            double gpuWattage = this.Gpu.CurrentPowerWatts ?? 0;
+            double vramTotalGb = Math.Round(this.Gpu.GetTotalVramBytes() / 1_073_741_824.0, 3);
+            double vramUsedGb = Math.Round(this.Gpu.GetUsedVramBytes() / 1_073_741_824.0, 3);
+
+            await Task.WhenAll(
+                this.UpdateCpuUsageAsync(threads),
+                this.UpdateRamUsageAsync(ramTotalGb, ramUsedGb),
+                this.UpdateGpuUsageAsync(gpuUsage, gpuWattage),
+                this.UpdateVramUsageAsync(vramTotalGb, vramUsedGb)
+            );
+        }
+
+        private void toolStripTextBox_interval_TextChanged(object sender, EventArgs e)
+        {
+            string text = this.toolStripTextBox_interval.Text;
+            if (int.TryParse(text, out int interval))
+            {
+                this._updateIntervalMs = interval;
+                this.UpdateTimer.Interval = this._updateIntervalMs < 50 ? 50 : this._updateIntervalMs;
+            }
+
+            this.toolStripTextBox_interval.Text = this._updateIntervalMs.ToString();
+        }
+
+
+        private async Task UpdateCpuUsageAsync(float[] usages)
+        {
+            var bmp = await CpuStats.RenderCoresBitmapAsync(usages, this.pictureBox_cpu.Width, this.pictureBox_cpu.Height, this._diagramColor, (this.showUsageToolStripMenuItem.Enabled ? this._percentageColor : null), CancellationToken.None);
+            this.Invoke((Action) (() =>
+            {
+                this.pictureBox_cpu.Image?.Dispose();
+                this.pictureBox_cpu.Image = bmp;
+            }));
+        }
+
+        private async Task UpdateRamUsageAsync(double totalGb, double usedGb)
+        {
+            double percentUsed = totalGb > 0 ? (usedGb / totalGb) * 100 : 0;
+
+            this.Invoke((Action) (() =>
+            {
+                this.label_ram.Text = $"RAM: {usedGb} GB / {totalGb} GB ({percentUsed:0.00}%)";
+                this.progressBar_ram.Value = Math.Clamp((int) percentUsed * 10, 0, this.progressBar_ram.Maximum);
+            }));
+
+            await Task.CompletedTask;
+        }
+
+        private async Task UpdateGpuUsageAsync(double usagePercent, double wattage)
+        {
+            this.Invoke((Action) (() =>
+            {
+                this.label_gpuUsage.Text = $"GPU: {usagePercent:0.00}%";
+                this.label_wattage.Text = $"Watts: {wattage:0.00} W";
+            }));
+
+            await Task.CompletedTask;
+        }
+
+        private async Task UpdateVramUsageAsync(double totalGb, double usedGb)
+        {
+            double percentUsed = totalGb > 0 ? (usedGb / totalGb) * 100 : 0;
+            this.Invoke((Action) (() =>
+            {
+                this.label_vram.Text = $"VRAM: {usedGb} GB / {totalGb} GB ({percentUsed:0.00}%)";
+                this.progressBar_vram.Value = Math.Clamp((int) percentUsed * 10, 0, this.progressBar_vram.Maximum);
+            }));
+
+            await Task.CompletedTask;
+        }
+
+        private void toolStripComboBox_gpus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Gpu?.Dispose();
+            this.Gpu = new GpuStats(this.toolStripComboBox_gpus.SelectedIndex);
+        }
+
+        private void toolStripTextBox_diagramColor_TextChanged(object sender, EventArgs e)
+        {
+            string hex = this.toolStripTextBox_diagramColor.Text.Replace("#", "");
+            if (hex.Length == 6 && int.TryParse(hex, System.Globalization.NumberStyles.HexNumber, null, out int rgb))
+            {
+                // rgb is RRGGBB; ensure the color is created opaque by adding alpha 0xFF
+                this._diagramColor = Color.FromArgb(unchecked((int)0xFF000000 | rgb));
+            }
+        }
+
+        private void toolStripTextBox_percentageColor_TextChanged(object sender, EventArgs e)
+        {
+            string hex = this.toolStripTextBox_percentageColor.Text.Replace("#", "");
+            if (hex.Length == 6 && int.TryParse(hex, System.Globalization.NumberStyles.HexNumber, null, out int rgb))
+            {
+                // rgb is RRGGBB; construct an opaque Color (add alpha byte)
+                this._percentageColor = Color.FromArgb(unchecked((int)0xFF000000 | rgb));
+            }
+            else
+            {
+                this._percentageColor = null;
+            }
+
+        }
+
+        private void toolStripTextBox_percentageColor_EnabledChanged(object sender, EventArgs e)
+        {
+            // Do not clear the stored color when disabling; keep the selected color so it can be re-enabled.
+            if (this.toolStripTextBox_percentageColor.Enabled)
+            {
+                this.toolStripTextBox_percentageColor.Text = this._percentageColor.HasValue ? $"#{this._percentageColor.Value.ToArgb() & 0xFFFFFF:X6}" : "";
+            }
+        }
+
+        private void toolStripTextBox_diagramColor_DoubleClick(object sender, EventArgs e)
+        {
+            // Color picker dialog
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                colorDialog.AllowFullOpen = true;
+                colorDialog.AnyColor = true;
+                colorDialog.SolidColorOnly = false;
+                colorDialog.Color = this._diagramColor;
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this._diagramColor = colorDialog.Color;
+                    this.toolStripTextBox_diagramColor.Text = $"#{colorDialog.Color.ToArgb() & 0xFFFFFF:X6}";
+                }
+            }
+        }
+
+        private void toolStripTextBox_percentageColor_DoubleClick(object sender, EventArgs e)
+        {
+            // Color picker dialog
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                colorDialog.AllowFullOpen = true;
+                colorDialog.AnyColor = true;
+                colorDialog.SolidColorOnly = false;
+                colorDialog.Color = this._percentageColor ?? Color.White;
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this._percentageColor = colorDialog.Color;
+                    this.toolStripTextBox_percentageColor.Text = $"#{colorDialog.Color.ToArgb() & 0xFFFFFF:X6}";
+                    this.toolStripTextBox_percentageColor.Enabled = true;
+                }
+            }
+        }
+    }
+}
